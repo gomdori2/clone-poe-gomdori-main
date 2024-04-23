1a1ba7eb15d2594cbe0d8aa79c9b841fc5d8ccbc
gomdori2 <dbstjdghks21@naver.com>
2024-04-23 22:26:01 +0900

    feature : slide 상단부 하단부 영역잡아놓음 css수정요망
    swiper : thumbnail로 상단/하단부 영역잡아놓음 css수정요망

--- public/www/js/slide.js
+++ public/www/js/slide.js
@@ -1,13 +1,33 @@
 window.addEventListener("load", function () {
+  //   var swiper = new Swiper(".game-slide", {
+  //     loop: true,
+  //     autoplay: {
+  //       delay: 2500,
+  //       disableOnInteraction: false,
+  //     },
+  //     navigation: {
+  //       nextEl: ".swiper-button-next",
+  //       prevEl: ".swiper-button-prev",
+  //     },
+  //   });
+  // });
+
   var swiper = new Swiper(".game-slide", {
     loop: true,
-    autoplay: {
-      delay: 2500,
-      disableOnInteraction: false,
-    },
+    spaceBetween: 10,
+    slidesPerView: 4,
+    freeMode: true,
+    watchSlidesProgress: true,
+  });
+  var swiper2 = new Swiper(".game-slide2", {
+    loop: true,
+    spaceBetween: 10,
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
+    },
+    thumbs: {
+      swiper: swiper,
     },
   });
 });
